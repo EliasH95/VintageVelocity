cffd464b73018e836f4a25a281eab9d6adc59edc
EliasH95 <EliasH95>
2024-11-14 22:04:08 +0100

    ogfonga mid dem dreg

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+"use strict";
+const vehiclesHeader = Document.getElementById("vehicles");
+document.addEventListener("DOMContentLoaded", () => {
+    const vehiclesHeader = Document.getElementById("vehicles");
+    vehiclesHeader.style.
+})
