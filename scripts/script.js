7cf61c3116a5df14c2b0abe108d536ea458d8b62
laditsch <114132011+EliasH95@users.noreply.github.com>
2025-02-13 19:44:33 +0100

    implemented members pageand first setup for about page

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,92 @@
 "use strict";
-const vehiclesHeader = Document.getElementById("vehicles");
-document.addEventListener("DOMContentLoaded", () => {
-    const vehiclesHeader = Document.getElementById("vehicles");
-    vehiclesHeader.style.
-})
+
+let vorstandDiv;
+let regularMemberDiv;
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const membersHeader = document.getElementById("membersHeader");
+
+    membersHeader.addEventListener("click", (event) => {
+        window.location = "/VintageVelocity/html/members.html";
+    });
+
+    const homeHeader = document.getElementById("homeHeader");
+
+    homeHeader.addEventListener("click", (event) => {
+        window.location = "/VintageVelocity/html/home.html";
+    });
+
+    const aboutHeader = document.getElementById("aboutHeader")
+
+    aboutHeader.addEventListener("click",(event) =>{
+        window.location = "/VintageVelocity/html/about.html";
+    })
+    console.log(aboutHeader);
+
+    if(window.location.href.includes("members")){
+        vorstandDiv = document.getElementById("executivesDiv");
+        regularMemberDiv = document.getElementById("regularMembersDiv");
+        buildMemberDivs();
+    }
+});
+
+async function loadMembers(){
+    const response = await fetch("/VintageVelocity/members.json");
+    const json = await response.json();
+    return json;
+}
+
+async function buildMemberDivs(){
+    const members = await loadMembers();
+    console.log(members)
+    console.log(Array.from(members.members));
+    Array.from(members.members).forEach(member => {
+        const memberDiv = document.createElement("div");
+
+        const img = document.createElement("img");
+        img.src = member.ImgPath;
+        img.style.height = "350px";
+        memberDiv.appendChild(img);
+
+        const nameP = document.createElement("p");
+            nameP.textContent = member.Name;
+            nameP.style.fontWeight = "bold";
+            memberDiv.appendChild(nameP);
+
+        const ageP = document.createElement("p");
+            const now = new Date();
+            const birthdate = new Date(member.Birthdate);
+            console.log(birthdate);
+            let age = now.getFullYear() - birthdate.getFullYear();
+            if (
+                now.getMonth() < birthdate.getMonth() ||
+                (now.getMonth() === birthdate.getMonth() && now.getDate() < birthdate.getDate())
+            ) {
+                age--;
+            }
+        
+            ageP.textContent = age;
+            memberDiv.appendChild(ageP);
+
+        const nationP = document.createElement("p");
+            nationP.textContent = member.Nationality;
+            memberDiv.appendChild(nationP);
+
+        const joinedP = document.createElement("p");
+            joinedP.textContent = member.Joined;
+            memberDiv.appendChild(joinedP);
+
+        const roleP = document.createElement("p");
+            roleP.textContent = member.Role;
+            //memberDiv.appendChild(roleP);
+
+        memberDiv.className = "memberDiv";
+        console.log(memberDiv);
+        String(member.Role).includes("Vorstand") ? 
+            vorstandDiv.append(memberDiv) :
+             regularMemberDiv.append(memberDiv);
+    });
+}
+
+
+
